86c3f88f600fbcfbca94835c9b08dcf5e28da488
Vojtech Matous <vojtech.matous@gmail.com>
2021-05-17 14:25:51 +0200

    stupid coaching rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  def ask
+
+  end
+
+  def answer
+    @question_query = params[:question]
+    # raise
+    # @answer = Time.now if @question_query = "what time it is?"
+    if @question_query == "I am going to work"
+      @answer = "Great!"
+    elsif @question_query[-1] == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # goes to the controlller I created before pages_controller.rb
+  # controller then aperated on the model ASIO
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
